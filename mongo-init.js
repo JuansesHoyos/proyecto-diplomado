35288a30435e30c91f0b849c0c1383bed7077fc3
JuanEsteban52002 <juane.grandar@uqvirtual.edu.co>
2024-08-30 19:13:18 -0500

    Indices y collection en DB
    Se agrego una collection (Login) y se agregaron dos indices para Usuario y Login

--- mongo-init.js
+++ mongo-init.js
@@ -5,5 +5,9 @@ const dbName = 'pruebaDB';
 db = db.getSiblingDB(dbName);
 
 db.createCollection('Usuario');
+db.createCollection('Login')
 db.createCollection('Firma');
 db.createCollection('Documento');
+
+db.Usuario.createIndex( { "user_id": 1 }, { unique: true } )
+db.Login.createIndex( { "login_id": 1 }, { unique: true } )
