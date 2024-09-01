54e6bf5a0c54c4bc600f4e81a388e5dc12f25886
JuansesHoyos <101542674+JuansesHoyos@users.noreply.github.com>
2024-09-01 00:25:46 -0500

    FileManagement
    Se creo una tabla y la función para cargar los archivos únicamente desde el front

--- frontProyectoFinal/src/app/components/file-management/file-management.component.ts
+++ frontProyectoFinal/src/app/components/file-management/file-management.component.ts
@@ -9,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class FileManagementComponent {
 
+  files: File[] = [];
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files) {
+      for (let i = 0; i < input.files.length; i++) {
+        this.files.push(input.files[i]);
+      }
+    }
+  }
+
+  uploadFiles(): void {
+    // Aquí puedes agregar la lógica para subir los archivos al servidor
+    console.log('Archivos subidos:', this.files);
+  }
+
 }
